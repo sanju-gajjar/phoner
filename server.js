05d71970a25973238fbab9c6cc3663f977d81cca
sanju_g <sanju.g@lendfoundry.com>
2023-05-01 15:06:45 +0530

    updated fix port to 5000

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const path = require('path');
 const keys = require('./config/keys');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
